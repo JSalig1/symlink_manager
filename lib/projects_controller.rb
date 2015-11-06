c4dfbfec50594900d7d1defdea7779cd880ff962
jsalig1 <jeremy.salig@gmail.com>
2015-11-06 11:51:04 -0500

    Refactor: Remove three middlemen objects

--- lib/projects_controller.rb
+++
@@ -1,7 +0,0 @@
-class ProjectsController
-  include Singleton
-
-  def index
-    DirectoryHelper.instance.find_all_project_directories
-  end
-end
--- symlink_manager.rb
+++ symlink_manager.rb
@@ -5,14 +5,11 @@ require 'pathname'
 require 'net/ssh'
 require './lib/directory_helper'
 require './lib/secure_shell'
-require './lib/directories_controller'
-require './lib/symlinks_controller'
-require './lib/projects_controller'
 
 Dotenv.load
 
 get "/" do
-  @directories = DirectoriesController.instance.index
+  @directories = DirectoryHelper.instance.all
   erb :index
 end
 
@@ -21,28 +18,28 @@ get "/user-folders/new" do
 end
 
 get "/user-folders/:user" do
-  @directory = DirectoriesController.instance.show(params[:user])
-  @symlinks = SymlinksController.instance.index(@directory)
+  @directory = DirectoryHelper.instance.find_by(params[:user])
+  @symlinks = DirectoryHelper.instance.find_symlinks_for(@directory)
   erb :show
 end
 
 get "/:user/new" do
   @user_folder = params[:user]
-  @project_directories = ProjectsController.instance.index
+  @project_directories = DirectoryHelper.instance.find_all_project_directories
   erb :new_symlink
 end
 
 post '/user-folders' do
-  DirectoriesController.instance.create(params[:folder_name])
+  DirectoryHelper.instance.new(params[:folder_name])
   redirect "/"
 end
 
 post '/:user/symlinks' do
-  SymlinksController.instance.create(params[:user], params[:symlink_name])
+  DirectoryHelper.instance.new_symlink(params[:user], params[:symlink_name])
   redirect "/"
 end
 
 delete '/:user/:symlink_name' do
-  SymlinksController.instance.destroy(params[:user], params[:symlink_name])
+  DirectoryHelper.instance.destroy_symlink(params[:user], params[:symlink_name])
   redirect "/"
 end
