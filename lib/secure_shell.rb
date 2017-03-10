8e33c8c39f2dd7c82aa9f069b67ad952f255d780
jsalig1 <jeremy.salig@gmail.com>
2017-03-10 13:40:15 -0500

    Fix connection closed by remote host.
    New update to remote FreeBSD server closes Net SSH connections if you
    try to use a 'none' encryption algorithm included in the gem. By specifying
    our own we can avoid this and also reduce packet sizes. Upgraded to the
    latest build as well to try and stay ahead of future server ugrades.

--- lib/secure_shell.rb
+++ lib/secure_shell.rb
@@ -3,6 +3,12 @@ class SecureShell
     @target = symlink_name
     @user = user
 
+    Net::SSH::Transport::Algorithms::ALGORITHMS[:encryption] = %w(
+      aes128-cbc 3des-cbc blowfish-cbc cast128-cbc
+      aes192-cbc aes256-cbc arcfour128 arcfour256 arcfour
+      aes128-ctr aes192-ctr aes256-ctr cast128-ctr blowfish-ctr 3des-ctr
+    )
+
     @session = Net::SSH.start(
       ENV['SERVER_HOST'],
       ENV['SERVER_USER'],
